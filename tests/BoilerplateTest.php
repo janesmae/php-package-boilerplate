e49c9b5edc8333070c2ec360d18cb632704c1f02
Jaan Janesmae <jaan@janesmae.com>
2016-02-14 16:45:23 +0200

    Initial commit

---
+++ tests/BoilerplateTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * PHP Package Boilerplate
+ * Boilerplate for PHP packages
+ *
+ * @link https://github.com/janesmae/php-package-boilerplate
+ * @version 0.1.0-dev
+ *
+ * @author Jaan Janesmae <jaan@janesmae.com>
+ * @copyright 2016 Jaan Janesmae
+ * @license MIT License (https://github.com/janesmae/php-package-boilerplate/blob/master/LICENSE)
+ */
+
+use janesmae\boilerplate\Boilerplate;
+
+class BoilerplateTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testBoilerplateInit()
+    {
+        $rsvp = new Boilerplate();
+        $this->assertTrue($rsvp->testInit());
+    }
+
+}
